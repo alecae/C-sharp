b6b0f64be7bf7c9d3db237982de5135cb5f87d33
alex <alex.caetano@ecole-isitech.fr>
2023-01-02 12:01:58 +0100

    Ajoutez des fichiers projet.

---
+++ C Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace ConsoleApp
+{
+    enum Mois
+    {
+        Janvier,
+        Fevrier,
+        Mars,
+        Avril,
+        Mai,
+        Juin,
+        Juillet,
+        Aout,
+        Septembre,
+        Octobre,
+        Novembre,
+        Decembre,
+    }
+    internal static class Program
+    {
+        public static void Main(string[] args )
+        {
+            Console.WriteLine("Hello, World!");
+
+            Mois Aujourdhui = Mois.Janvier;
+            Mois AujourdhuiB = 0; 
+
+            Console.WriteLine(Aujourdhui);
+            Console.WriteLine(AujourdhuiB);
+        }
+
+
+    }
+}
+
