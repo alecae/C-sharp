d4f2a5fd3781f2b30be629dd25aaad55d7088e6b
alex <alex.caetano@ecole-isitech.fr>
2023-01-02 13:57:49 +0100

    tableau

--- C Sharp/Class1.cs
+++ C Sharp/Class1.cs
@@ -1,13 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace C_Sharp
-{
-    internal class Class1
-    {
+//namespace ConsoleApp
+//{
+//    enum Mois
+//    {
+//        Janvier,
+//        Fevrier,
+//        Mars,
+//        Avril,
+//        Mai,
+//        Juin,
+//        Juillet,
+//        Aout,
+//        Septembre,
+//        Octobre,
+//        Novembre,
+//        Decembre
+//    }
+//    internal static class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
 
-    }
-}
+//            Mois Aujourdhui = Mois.Janvier;
+//            Mois AujourdhuiB = 0;
+
+//            Console.WriteLine(Aujourdhui);
+//            Console.WriteLine(AujourdhuiB);
+
+//            Mois Vacances = Mois.Juillet | Mois.Aout;
+
+//            Console.WriteLine(Vacances);
+
+
+//        }
+
+
+//        int[][] Tableau = new int[3][];
+//        Tabelau[0] =  new int[] {45,2};
+//        Tableau[1] = new int[] {34, 34, 4, 67}
+
+//    }
+//}
---
+++ C Sharp/exoTableau.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+
+
+namespace C_Sharp
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            int[,] Tableau = new int[1, 2];
+            int[,,] Tableau2 = new int[5, 2, 3];
+
+            foreach (var i in Tableau)
+            {
+                Console.WriteLine(i);
+            }
+            //on affiche le premier tableau qui fait apparaitre un zero pour chaque valeur disponible 
+
+            foreach (var i in Tableau2)
+            {
+                Console.WriteLine(i);
+            }
+            //on affiche le deuxieme tableau qui fait exactement comme la premier mais avec plus de valeur  
+
+
+            int[][] Tableau3 = new int[3][];
+            Tableau3[0] =  new int[] {45,2};
+            Tableau3[1] = new int[] { 34, 34, 4, 67 };
+
+            foreach (var i in Tableau3[0])
+            {
+                Console.WriteLine(i);
+            }
+            //on affiche le premier tableau avec ces differentes valeurs (45 et 2)
+
+            foreach (var i in Tableau3[1])
+            {
+                Console.WriteLine(i);
+            }
+            //on affiche le deuxieme tableau avec ces differentes valeurs 
+
+        }
+    }
+}
+
