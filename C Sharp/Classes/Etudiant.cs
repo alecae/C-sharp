61f5f989a8d3d350ba3be3468be1fd339a82f92d
alex <alex.caetano@ecole-isitech.fr>
2023-01-03 17:23:33 +0100

    debut jeux

--- C Sharp/Classes/Etudiant.cs
+++ C Sharp/Classes/Etudiant.cs
@@ -8,12 +8,12 @@ namespace C_Sharp.Classes
 {
     public class Etudiant
     {
-        // propriétés de la classe
+
         public string nom { get; set; }
         public string prenom { get; set; }
         public int age { get; set; }
 
-        // constructeur de la classe
+
         public Etudiant(string nom, string prenom, int age)
         {
             this.nom = nom;
@@ -21,13 +21,12 @@ namespace C_Sharp.Classes
             this.age = age;
         }
 
-        // méthode qui retourne le nom complet de l'étudiant
+
         public string NomComplet()
         {
             return prenom + " " + nom;
         }
 
-        // méthode qui retourne true si l'étudiant est majeur, false sinon
         public bool EstMajeur()
         {
             return age >= 18;
